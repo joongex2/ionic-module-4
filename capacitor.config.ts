6af49242f34ae58e14b730dae6a7b6c379828888
joongex3 <s.auengsirirat@gmail.com>
2023-07-15 15:01:01 +0700

    fix: fix

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.udemy-course.ionic-angular',
+  appName: 'module-4',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
